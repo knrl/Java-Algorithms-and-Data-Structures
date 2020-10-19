6f5ec7c2fb89d010240da62c85c738f90452fdc8
Mehmet Kaan EROL <mehmetkaanerol@Mehmets-MacBook-Pro.local>
2020-10-19 22:23:45 +0300

    #1

---
+++ sorting-algorithms/selectionSort/selectionSort.java
@@ -0,0 +1,31 @@
+
+class selectionSort{
+	public static void main(String [] args){
+		int [] arr = {1, 3, 5, 6, 6, 1, 3, 5};
+		int sizeofArr = arr.length;
+		int [] sortedArr = new int [sizeofArr];
+		sortedArr = selectionSort(arr, sizeofArr);
+		yazdir(sortedArr);
+	}
+
+	static int [] selectionSort(int [] arr, int sizeofArr){
+		int minIndex=0, temp=0;
+		for (int i=0;i<sizeofArr;i++){
+			minIndex = i;
+			for (int k=i+1;k<sizeofArr;k++)
+				if (arr[minIndex] > arr[k]) minIndex = k;
+			if (minIndex != i){
+				temp = arr[minIndex];
+				arr[minIndex] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		return arr;
+	}
+
+	static void yazdir (int [] arr){
+		for (int a : arr)
+			System.out.print(" "+a);
+
+	}
+}
