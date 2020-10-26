799d4e1f88eef70940417ecd0b06b77d1debe3e9
Mehmet Kaan EROL <mehmetkaanerol@Mehmets-MacBook-Pro.local>
2020-10-26 12:04:20 +0300

    #1

---
+++ sorting-algorithms/bucketSort/bucketSort.java
@@ -0,0 +1,39 @@
+
+class bucketSort
+	public static void main(String [] args){
+		float [] arr = {1, 3, 5, 6, 6, 1, 3, 5};
+		int sizeofArr = arr.length;
+		float [] sortedArr = new float [sizeofArr];
+		sortedArr = bucketSort(arr, sizeofArr);
+	}
+
+	static float [] bucketSort(float [] arr, int sizeofArr){
+		int j=0;
+		arr = normaliseIt(arr, n);
+		Vector<Int>[] buckets = new Vector[10];
+		for (int i=0;i<10;i++) buckets[i] = new Vector<Int>();
+		for (int i=0;i<n;i++) buckets[(int) arr[i]*10].add(arr[i]);
+		for (int i=0;i<10;i++) Collections.sort(buckets[i]);
+		for (int i=0;i<n;i++) for (int k=0;k<buckets[i].size();k++)
+			arr[j++] = buckets[i].get(k);
+		return deNormaliseIt(arr);
+		
+	}
+
+	static float [] denormaliseIt(float [] arr, int n){
+		float max = getMax(arr, n);
+		for (int i=0;i<n;i++) arr[i] *= max;
+		return arr;
+	}
+
+	static float [] normaliseIt(float [] arr, int n){
+		float max = getMax(arr, n);
+		for (int i=0;i<n;i++) arr[i] /= max;
+		return arr;
+	}
+
+	static float getMax(float [] arr, int n){
+		float max = 0;
+		for (int i=0;i<n;i++) if (max < arr[i]) max = arr[i];
+	}
+}
